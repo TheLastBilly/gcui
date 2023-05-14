8ea72abbc5c31f689452e22367154c3e4d079152
TheLastBilly <jtmonegro@gmail.com>
2023-05-14 13:22:51 -0400

    updated scripts

--- scripts/updater.py
+++ scripts/updater.py
@@ -1,27 +1,33 @@
+import os
 import serial
 import signal
 import sys
 import time
-
-def signal_handler(sig, frame):
-    sys.exit(0)
 
 def write_to_file(filename, s):
     print(filename + ": " + s)
     with open(filename, "w") as f:
         f.write(s)
 
+if(len(sys.argv) < 3):
+	print("Usage: updater.py PORT SOCKET_DIR")
+	sys.exit(1)
+
+ser = serial.Serial(sys.argv[1], 9600, timeout=0)
+def signal_handler(sig, frame):
+    ser.close()
+    sys.exit(0)
+    
 signal.signal(signal.SIGINT, signal_handler)
 battery = ""
 speedometer = ""
 temperature = ""
 
-ser = serial.Serial(sys.argv[1], 9600, timeout=0)
 while True:
-    if(not ser.is_open):
-        ser = serial.Serial(sys.argv[1], 9600, timeout=0)
     line = ""
     try:
+        if(not ser.is_open):
+            ser = serial.Serial(sys.argv[1], 9600, timeout=0)
         raw = ser.readline()
         if(len(raw) < 3):
             continue
@@ -37,7 +43,7 @@ while True:
     speedometer = str(int(float(fields[1]) * 80.0))
     temperature = fields[2]
 
-    write_to_file(argv[2] + "/battery", battery)
-    write_to_file(argv[2] + "/speed", speedometer)
-    write_to_file(argv[2] + "/temperature", temperature)
+    write_to_file(os.path.join(sys.argv[2], "battery"), battery)
+    write_to_file(os.path.join(sys.argv[2], "speed"), speedometer)
+    write_to_file(os.path.join(sys.argv[2], "temperature"), temperature)
     time.sleep(0.1)
