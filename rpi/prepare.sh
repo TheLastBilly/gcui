a3432322b85e76ad713a04252fa78101cf161096
drevil <drevil@belen.lan>
2023-06-01 23:00:13 -0400

    now launching gcui from inittab

--- rpi/prepare.sh
+++ rpi/prepare.sh
@@ -35,6 +35,29 @@ gpu_mem=100
 dtoverlay=miniuart-bt,krnbt=on
 dtoverlay=vc4-kms-v3d" > buildroot/output/images/rpi-firmware/config.txt
 
+echo "# /etc/inittab
+::sysinit:/bin/mount -t proc proc /proc
+::sysinit:/bin/mount -o remount,rw /
+::sysinit:/bin/mkdir -p /dev/pts /dev/shm
+::sysinit:/bin/mount -a
+::sysinit:/sbin/swapon -a
+null::sysinit:/bin/ln -sf /proc/self/fd /dev/fd
+null::sysinit:/bin/ln -sf /proc/self/fd/0 /dev/stdin
+null::sysinit:/bin/ln -sf /proc/self/fd/1 /dev/stdout
+null::sysinit:/bin/ln -sf /proc/self/fd/2 /dev/stderr
+::sysinit:/bin/hostname -F /etc/hostname
+
+::sysinit:/etc/init.d/rcS
+
+console::respawn:/sbin/getty -L  console 0 vt100 # GENERIC_SERIAL
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console
+::sysinit:nohup reader -s /dev/ttyACM0 -p /tmp &
+::sysinit:nohup gcui -x 1280 -y 720 -s /tmp -f &
+
+::shutdown:/etc/init.d/rcK
+::shutdown:/sbin/swapoff -a
+::shutdown:/bin/umount -a -r" > buildroot/output/target/etc/inittab 
+
 # Make image
 cd buildroot/
 make -j4
