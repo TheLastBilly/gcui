f308bbda120da6f5d2bec72a9517f32063ae2907
drevil <drevil@belen.lan>
2023-06-01 22:01:30 -0400

    reader now using simple open reads

--- reader.c
+++ reader.c
@@ -71,64 +71,6 @@ swrite( const char * s, const char * str )
     return 0;
 }
 
-int
-set_interface_attribs (int fd, int speed, int parity)
-{
-        struct termios tty;
-        if (tcgetattr (fd, &tty) != 0)
-        {
-                exit(1);
-                return -1;
-        }
-
-        cfsetospeed (&tty, speed);
-        cfsetispeed (&tty, speed);
-
-        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-        // disable IGNBRK for mismatched speed tests; otherwise receive break
-        // as \000 chars
-        tty.c_iflag &= ~IGNBRK;         // disable break processing
-        tty.c_lflag = 0;                // no signaling chars, no echo,
-                                        // no canonical processing
-        tty.c_oflag = 0;                // no remapping, no delays
-        tty.c_cc[VMIN]  = 0;            // read doesn't block
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
-
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
-
-        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
-                                        // enable reading
-        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-        tty.c_cflag |= parity;
-        tty.c_cflag &= ~CSTOPB;
-        tty.c_cflag &= ~CRTSCTS;
-
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-        {
-                exit(1);
-                return -1;
-        }
-        return 0;
-}
-
-void
-set_blocking (int fd, int should_block)
-{
-        struct termios tty;
-        memset (&tty, 0, sizeof tty);
-        if (tcgetattr (fd, &tty) != 0)
-        {
-                exit(1);
-                return;
-        }
-
-        tty.c_cc[VMIN]  = should_block ? 1 : 0;
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
-
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-            exit(1);
-}
-
 static inline void
 sigint(int n)
 {
@@ -158,28 +100,20 @@ int main(int argc, char *argv[])
    		}
    	}
 
-recon:
-    fd = open (spath, O_RDWR | O_SYNC | O_NONBLOCK);
+    fd = open (spath, O_RDONLY);
     if (fd < 0)
     {
-    	printf("failed to open socket: %s\n", strerror(errno));
+    	printf("failed to open socket: %s\n\n", strerror(errno));
         return 1;
     }
-
-    set_interface_attribs(fd, B9600, 0);
-    set_blocking(fd, 0);
-
-    usleep ((7 + 25) * 100);
-    
+    printf("connected to \"%s\"\n", spath);
+ 
     while(shouldrun)
     {
-    	r = read(fd, &buf[t], sizeof(buf) - t);
-    	if(r <= 0)
+    	r = read(fd, &buf[t], sizeof(buf) -1);
+    	if(r < 0)
     	{
-    		r = 0;
-    		//printf("failed to read from socket: %s\n", strerror(errno));
-    		close(fd);
-    		goto recon;
+            continue;
     	}
     	else
     	{
@@ -187,7 +121,6 @@ recon:
     		if(t >= sizeof(buf))
     		{
     			t = 0;
-    			tcflush(fd, TCIOFLUSH);
     			continue;
     		}
     	}
@@ -216,6 +149,8 @@ recon:
     	}
     	t = 0;
     }
-	
+    
+    if(fd > 0)
+        close(fd);
     return 0;
 }
